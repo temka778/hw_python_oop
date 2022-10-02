653b37a6107cf93dcb8d5a54d4d61793958af7a8
Артём <av1965k@yandex.ru>
2022-10-02 04:23:57 +0400

    Убрал лишнюю функцию, добавил описание в метод вывода объекта инфомесседж, добавил трай и эксцепт

--- homework.py
+++ homework.py
@@ -140,4 +140,4 @@ if __name__ == '__main__':
             training = read_package(workout_type, data)
             main(training)
         except KeyError:
-            print('Нет такого типа тренировок...')
+            print('Нет такого типа тренировок.')
